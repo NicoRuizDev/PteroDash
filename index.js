2a6b2be32bd9e969eb356921e4568afb25bbd7ea
Nico Ruiz <lifegamer2050@gmail.com>
2023-03-10 15:48:02 +0530

    update

--- index.js
+++ index.js
@@ -3,9 +3,9 @@
 // Load packages.
 
 const fs = require("fs");
-const fetch = require('node-fetch');
+const fetch = require("node-fetch");
 const chalk = require("chalk");
-const arciotext = (require("./api/arcio.js")).text;
+const arciotext = require("./backend/arcio.js").text;
 
 // Load settings.
 
@@ -18,11 +18,10 @@ const defaultthemesettings = {
   pages: {},
   mustbeloggedin: [],
   mustbeadmin: [],
-  variables: {}
+  variables: {},
 };
 
-module.exports.renderdataeval =
-  `(async () => {
+module.exports.renderdataeval = `(async () => {
     let newsettings = JSON.parse(require("fs").readFileSync("./settings.json"));
 
     const JavaScriptObfuscator = require('javascript-obfuscator');
@@ -78,7 +77,7 @@ const app = express();
 
 // Load express addons.
 
-const expressWs = require('express-ws')(app);
+const expressWs = require("express-ws")(app);
 const ejs = require("ejs");
 const session = require("express-session");
 const indexjs = require("./index.js");
@@ -93,45 +92,57 @@ const session_db = new sqlite("sessions.db");
 
 module.exports.app = app;
 
-app.use(session({
-  secret: settings.website.secret,
-  resave: true,
-  saveUninitialized: true,
-  store: new SqliteStore({
-    client: session_db, 
-    expired: {
-      clear: true,
-      intervalMs: 900000
-    }
+app.use(
+  session({
+    secret: settings.website.secret,
+    resave: true,
+    saveUninitialized: true,
+    store: new SqliteStore({
+      client: session_db,
+      expired: {
+        clear: true,
+        intervalMs: 900000,
+      },
+    }),
   })
-}));
+);
 
-app.use(express.json({
-  inflate: true,
-  limit: '500kb',
-  reviver: null,
-  strict: true,
-  type: 'application/json',
-  verify: undefined
-}));
+app.use(
+  express.json({
+    inflate: true,
+    limit: "500kb",
+    reviver: null,
+    strict: true,
+    type: "application/json",
+    verify: undefined,
+  })
+);
 
-const listener = app.listen(settings.website.port, function() {
-  console.log(chalk.green("[WEBSITE] The dashboard has successfully loaded on port " + listener.address().port + "."));
+const listener = app.listen(settings.website.port, function () {
+  console.log(
+    chalk.green(
+      "[WEBSITE] The dashboard has successfully loaded on port " +
+        listener.address().port +
+        "."
+    )
+  );
 });
 
 let ipratelimit = {};
 
 var cache = 0;
 
-setInterval(
-  async function() {
-    if (cache - .1 < 0) return cache = 0;
-    cache = cache - .1;
-  }, 100
-)
+setInterval(async function () {
+  if (cache - 0.1 < 0) return (cache = 0);
+  cache = cache - 0.1;
+}, 100);
 
 app.use(async (req, res, next) => {
-  if (req.session.userinfo && req.session.userinfo.id && !(await db.get("users-" + req.session.userinfo.id))) {
+  if (
+    req.session.userinfo &&
+    req.session.userinfo.id &&
+    !(await db.get("users-" + req.session.userinfo.id))
+  ) {
     let theme = indexjs.get(req);
 
     req.session.destroy(() => {
@@ -156,7 +167,7 @@ app.use(async (req, res, next) => {
     "/remove_account": 1,
     "/create_coupon": 1,
     "/revoke_coupon": 1,
-    "/getip": 1
+    "/getip": 1,
   };
   if (manager[req._parsedUrl.pathname]) {
     if (cache > 0) {
@@ -168,174 +179,245 @@ app.use(async (req, res, next) => {
         }
         querystring = "?" + querystring.slice(1);
         if (querystring == "?") querystring = "";
-        res.redirect((req._parsedUrl.pathname.slice(0, 1) == "/" ? req._parsedUrl.pathname : "/" + req._parsedUrl.pathname) + querystring);
+        res.redirect(
+          (req._parsedUrl.pathname.slice(0, 1) == "/"
+            ? req._parsedUrl.pathname
+            : "/" + req._parsedUrl.pathname) + querystring
+        );
       }, 1000);
       return;
     } else {
-      let newsettings = JSON.parse(fs.readFileSync("./settings.json").toString());
+      let newsettings = JSON.parse(
+        fs.readFileSync("./settings.json").toString()
+      );
 
       if (newsettings.api.client.ratelimits.enabled == true) {
+        let ip =
+          newsettings.api.client.ratelimits["trust x-forwarded-for"] == true
+            ? req.headers["x-forwarded-for"] || req.connection.remoteAddress
+            : req.connection.remoteAddress;
+        ip = (ip ? ip : "::1")
+          .replace(/::1/g, "::ffff:127.0.0.1")
+          .replace(/^.*:/, "");
 
-        let ip = (newsettings.api.client.ratelimits["trust x-forwarded-for"] == true ? (req.headers['x-forwarded-for'] || req.connection.remoteAddress) : req.connection.remoteAddress);
-        ip = (ip ? ip : "::1").replace(/::1/g, "::ffff:127.0.0.1").replace(/^.*:/, '');
-      
-        if (ipratelimit[ip] && ipratelimit[ip] >= newsettings.api.client.ratelimits.requests) {
+        if (
+          ipratelimit[ip] &&
+          ipratelimit[ip] >= newsettings.api.client.ratelimits.requests
+        ) {
           // possibly add a custom theme for this in the future
-          res.send(`<html><head><title>You are being rate limited.</title></head><body>You have exceeded rate limits.</body></html>`);
+          res.send(
+            `<html><head><title>You are being rate limited.</title></head><body>You have exceeded rate limits.</body></html>`
+          );
           return;
         }
-      
+
         ipratelimit[ip] = (ipratelimit[ip] ? ipratelimit[ip] : 0) + 1;
-      
-        setTimeout(
-          async function() {
-            ipratelimit[ip] = ipratelimit[ip] - 1;
-            if (ipratelimit[ip] <= 0) ipratelimit[ip] = 0;
-          }, newsettings.api.client.ratelimits["per second"] * 1000
-        );
-  
-      };
+
+        setTimeout(async function () {
+          ipratelimit[ip] = ipratelimit[ip] - 1;
+          if (ipratelimit[ip] <= 0) ipratelimit[ip] = 0;
+        }, newsettings.api.client.ratelimits["per second"] * 1000);
+      }
 
       cache = cache + manager[req._parsedUrl.pathname];
     }
-  };
+  }
   next();
 });
 
 // Load the API files.
 
-let apifiles = fs.readdirSync('./api').filter(file => file.endsWith('.js'));
+let apifiles = fs
+  .readdirSync("./backend")
+  .filter((file) => file.endsWith(".js"));
 
-apifiles.forEach(file => {
-  let apifile = require(`./api/${file}`);
-	apifile.load(app, db);
+apifiles.forEach((file) => {
+  let apifile = require(`./backend/${file}`);
+  apifile.load(app, db);
 });
 
 app.all("*", async (req, res) => {
-  if (req.session.pterodactyl) if (req.session.pterodactyl.id !== await db.get("users-" + req.session.userinfo.id)) return res.redirect("/login?prompt=none");
+  if (req.session.pterodactyl)
+    if (
+      req.session.pterodactyl.id !==
+      (await db.get("users-" + req.session.userinfo.id))
+    )
+      return res.redirect("/login?prompt=none");
   let theme = indexjs.get(req);
 
   let newsettings = JSON.parse(require("fs").readFileSync("./settings.json"));
-  if (newsettings.api.arcio.enabled == true) if (theme.settings.generateafktoken.includes(req._parsedUrl.pathname)) req.session.arcsessiontoken = Math.random().toString(36).substring(2, 15);
-  
-  if (theme.settings.mustbeloggedin.includes(req._parsedUrl.pathname)) if (!req.session.userinfo || !req.session.pterodactyl) return res.redirect("/login" + (req._parsedUrl.pathname.slice(0, 1) == "/" ? "?redirect=" + req._parsedUrl.pathname.slice(1) : ""));
+  if (newsettings.api.arcio.enabled == true)
+    if (theme.settings.generateafktoken.includes(req._parsedUrl.pathname))
+      req.session.arcsessiontoken = Math.random().toString(36).substring(2, 15);
+
+  if (theme.settings.mustbeloggedin.includes(req._parsedUrl.pathname))
+    if (!req.session.userinfo || !req.session.pterodactyl)
+      return res.redirect(
+        "/login" +
+          (req._parsedUrl.pathname.slice(0, 1) == "/"
+            ? "?redirect=" + req._parsedUrl.pathname.slice(1)
+            : "")
+      );
   if (theme.settings.mustbeadmin.includes(req._parsedUrl.pathname)) {
     ejs.renderFile(
-      `./themes/${theme.name}/${theme.settings.notfound}`, 
+      `./frontend/${theme.name}/${theme.settings.notfound}`,
       await eval(indexjs.renderdataeval),
       null,
-    async function (err, str) {
-      delete req.session.newaccount;
-      delete req.session.password;
-      if (!req.session.userinfo || !req.session.pterodactyl) {
-        if (err) {
-          console.log(chalk.red(`[WEBSITE] An error has occured on path ${req._parsedUrl.pathname}:`));
-          console.log(err);
-          return res.send("An error has occured while attempting to load this page. Please contact an administrator to fix this.");
-        };
-        res.status(404);
-        return res.send(str);
-      };
-
-      let cacheaccount = await fetch(
-        settings.pterodactyl.domain + "/api/application/users/" + (await db.get("users-" + req.session.userinfo.id)) + "?include=servers",
-        {
-          method: "get",
-          headers: { 'Content-Type': 'application/json', "Authorization": `Bearer ${settings.pterodactyl.key}` }
-        }
-      );
-      if (await cacheaccount.statusText == "Not Found") {
-        if (err) {
-          console.log(chalk.red(`[WEBSITE] An error has occured on path ${req._parsedUrl.pathname}:`));
-          console.log(err);
-          return res.send("An error has occured while attempting to load this page. Please contact an administrator to fix this.");
-        };
-        return res.send(str);
-      };
-      let cacheaccountinfo = JSON.parse(await cacheaccount.text());
-    
-      req.session.pterodactyl = cacheaccountinfo.attributes;
-      if (cacheaccountinfo.attributes.root_admin !== true) {
-        if (err) {
-          console.log(chalk.red(`[WEBSITE] An error has occured on path ${req._parsedUrl.pathname}:`));
-          console.log(err);
-          return res.send("An error has occured while attempting to load this page. Please contact an administrator to fix this.");
-        };
-        return res.send(str);
-      };
-
-      ejs.renderFile(
-        `./themes/${theme.name}/${theme.settings.pages[req._parsedUrl.pathname.slice(1)] ? theme.settings.pages[req._parsedUrl.pathname.slice(1)] : theme.settings.notfound}`, 
-        await eval(indexjs.renderdataeval),
-        null,
-      function (err, str) {
+      async function (err, str) {
         delete req.session.newaccount;
         delete req.session.password;
-        if (err) {
-          console.log(`[WEBSITE] An error has occured on path ${req._parsedUrl.pathname}:`);
-          console.log(err);
-          return res.send("An error has occured while attempting to load this page. Please contact an administrator to fix this.");
-        };
-        res.status(404);
-        res.send(str);
-      });
-    });
+        if (!req.session.userinfo || !req.session.pterodactyl) {
+          if (err) {
+            console.log(
+              chalk.red(
+                `[WEBSITE] An error has occured on path ${req._parsedUrl.pathname}:`
+              )
+            );
+            console.log(err);
+            return res.send(
+              "An error has occured while attempting to load this page. Please contact an administrator to fix this."
+            );
+          }
+          res.status(404);
+          return res.send(str);
+        }
+
+        let cacheaccount = await fetch(
+          settings.pterodactyl.domain +
+            "/api/application/users/" +
+            (await db.get("users-" + req.session.userinfo.id)) +
+            "?include=servers",
+          {
+            method: "get",
+            headers: {
+              "Content-Type": "application/json",
+              Authorization: `Bearer ${settings.pterodactyl.key}`,
+            },
+          }
+        );
+        if ((await cacheaccount.statusText) == "Not Found") {
+          if (err) {
+            console.log(
+              chalk.red(
+                `[WEBSITE] An error has occured on path ${req._parsedUrl.pathname}:`
+              )
+            );
+            console.log(err);
+            return res.send(
+              "An error has occured while attempting to load this page. Please contact an administrator to fix this."
+            );
+          }
+          return res.send(str);
+        }
+        let cacheaccountinfo = JSON.parse(await cacheaccount.text());
+
+        req.session.pterodactyl = cacheaccountinfo.attributes;
+        if (cacheaccountinfo.attributes.root_admin !== true) {
+          if (err) {
+            console.log(
+              chalk.red(
+                `[WEBSITE] An error has occured on path ${req._parsedUrl.pathname}:`
+              )
+            );
+            console.log(err);
+            return res.send(
+              "An error has occured while attempting to load this page. Please contact an administrator to fix this."
+            );
+          }
+          return res.send(str);
+        }
+
+        ejs.renderFile(
+          `./frontend/${theme.name}/${
+            theme.settings.pages[req._parsedUrl.pathname.slice(1)]
+              ? theme.settings.pages[req._parsedUrl.pathname.slice(1)]
+              : theme.settings.notfound
+          }`,
+          await eval(indexjs.renderdataeval),
+          null,
+          function (err, str) {
+            delete req.session.newaccount;
+            delete req.session.password;
+            if (err) {
+              console.log(
+                `[WEBSITE] An error has occured on path ${req._parsedUrl.pathname}:`
+              );
+              console.log(err);
+              return res.send(
+                "An error has occured while attempting to load this page. Please contact an administrator to fix this."
+              );
+            }
+            res.status(404);
+            res.send(str);
+          }
+        );
+      }
+    );
     return;
-  };
+  }
   ejs.renderFile(
-    `./themes/${theme.name}/${theme.settings.pages[req._parsedUrl.pathname.slice(1)] ? theme.settings.pages[req._parsedUrl.pathname.slice(1)] : theme.settings.notfound}`, 
+    `./frontend/${theme.name}/${
+      theme.settings.pages[req._parsedUrl.pathname.slice(1)]
+        ? theme.settings.pages[req._parsedUrl.pathname.slice(1)]
+        : theme.settings.notfound
+    }`,
     await eval(indexjs.renderdataeval),
     null,
-  function (err, str) {
-    delete req.session.newaccount;
-    delete req.session.password;
-    if (err) {
-      console.log(chalk.red(`[WEBSITE] An error has occured on path ${req._parsedUrl.pathname}:`));
-      console.log(err);
-      return res.send("An error has occured while attempting to load this page. Please contact an administrator to fix this.");
-    };
-    res.status(404);
-    res.send(str);
-  });
+    function (err, str) {
+      delete req.session.newaccount;
+      delete req.session.password;
+      if (err) {
+        console.log(
+          chalk.red(
+            `[WEBSITE] An error has occured on path ${req._parsedUrl.pathname}:`
+          )
+        );
+        console.log(err);
+        return res.send(
+          "An error has occured while attempting to load this page. Please contact an administrator to fix this."
+        );
+      }
+      res.status(404);
+      res.send(str);
+    }
+  );
 });
 
-module.exports.get = function(req) {
-  let defaulttheme = JSON.parse(fs.readFileSync("./settings.json")).defaulttheme;
+module.exports.get = function (req) {
+  let defaulttheme = JSON.parse(
+    fs.readFileSync("./settings.json")
+  ).defaulttheme;
   let tname = encodeURIComponent(getCookie(req, "theme"));
-  let name = (
-    tname ?
-      fs.existsSync(`./themes/${tname}`) ?
-        tname
+  let name = tname
+    ? fs.existsSync(`./frontend/${tname}`)
+      ? tname
       : defaulttheme
-    : defaulttheme
-  )
+    : defaulttheme;
   return {
-    settings: (
-      fs.existsSync(`./themes/${name}/pages.json`) ?
-        JSON.parse(fs.readFileSync(`./themes/${name}/pages.json`).toString())
-      : defaultthemesettings
-    ),
-    name: name
+    settings: fs.existsSync(`./frontend/${name}/pages.json`)
+      ? JSON.parse(fs.readFileSync(`./frontend/${name}/pages.json`).toString())
+      : defaultthemesettings,
+    name: name,
   };
 };
 
-module.exports.islimited = async function() {
+module.exports.islimited = async function () {
   return cache <= 0 ? true : false;
-}
+};
 
-module.exports.ratelimits = async function(length) {
-  cache = cache + length
-}
+module.exports.ratelimits = async function (length) {
+  cache = cache + length;
+};
 
 // Get a cookie.
 function getCookie(req, cname) {
   let cookies = req.headers.cookie;
   if (!cookies) return null;
   let name = cname + "=";
-  let ca = cookies.split(';');
+  let ca = cookies.split(";");
   for (let i = 0; i < ca.length; i++) {
     let c = ca[i];
-    while (c.charAt(0) == ' ') {
+    while (c.charAt(0) == " ") {
       c = c.substring(1);
     }
     if (c.indexOf(name) == 0) {
